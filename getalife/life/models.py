a0d7a47a84400d688850f5378e207435f9f8bff9
jzaikin <jzaikin@msudenver.edu>
2020-02-19 12:47:35 -0700

    Fix Migrations + Add Month Enum + Fix python wierdness

--- getalife/life/models.py
+++ getalife/life/models.py
@@ -1,53 +1,57 @@
 from django.conf import settings
 from django.db import models
+from datetime import date
 
-# Create your models here.
-
-
+# models:
+class Month(models.IntegerChoices):
+    January = 0
+    February = 1
+    March = 2
+    April = 3
+    May = 4
+    June = 5
+    July = 6
+    August = 7
+    September = 8 
+    October = 9
+    November = 10
+    December = 11
 
 # financial_management_budget
 class Budget_account(models.Model):
     # Many-Many relationship to user model
     users = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=150)
-    created_utc = models.DateField(auto_now=False, auto_now_add=True, USE_TZ=False)
-    modefied_utc = models.DateField(auto_now=True, auto_now_add=False, USE_TZ=False)
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=150, null=False)
+    created_utc = models.DateField(auto_now_add=True, null=False)
+    modefied_utc = models.DateField(auto_now=True, null=False)
 
+# financial_management_budget_expense_type
+class Expense_type(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    created_utc = models.DateField(auto_now_add=True, null=False)
+    modefied_utc = models.DateField(auto_now=True, null=False)
+
+# financial_management_budget_income
+class Income_type(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    created_utc = models.DateField(auto_now_add=True, null=False)
+    modefied_utc = models.DateField(auto_now=True, null=False)
 
 # financial_management_budget_expense
 class Budget_expense(models.Model):
     budget = models.ForeignKey(Budget_account, on_delete=models.CASCADE)
-    # expense_type = models.ForeignKey(expense_type, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
-    month = models.DateField(auto_now=False, auto_now_add=False)
-# description: nvarchar(150)
-# month int (or 4 bits)
-# created (datetime(6))
-# modified(datetime(6))
+    expenseType = models.ForeignKey(Expense_type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=50, null=False)
+    month = models.IntegerField(choices=Month.choices)
+    created_utc = models.DateField(auto_now_add=True, null=False)
+    modefied_utc = models.DateField(auto_now=True, null=False)
 
-
-# financial_management_budget_expense_type
-# type int
-# description nvarchar(50)
-# created: datetime(6)
-# modfied: datetime(6)
-
-# financial_management_budget_income
-# id UUID
-# budget_id UUID
-# name nvarchar(50)
-# expense_type: int
-# description: nvarchar(150)
-# month int (or 4 bits)
-# created (datetime(6))
-# modified(datetime(6))
-
-# financial_management_budget_income_type
-# type int
-# description nvarchar(50)
-# created: datetime(6)
-# modfied: datetime(6)
 
 # financial_management_budget_config
+class Budget_config(models.Model):
+    budget = models.ForeignKey(Budget_account, on_delete=models.CASCADE)
+    month = models.IntegerField(choices=Month.choices)
+    created_utc = models.DateField(auto_now_add=True, null=False)
+    modefied_utc = models.DateField(auto_now=True, null=False)
