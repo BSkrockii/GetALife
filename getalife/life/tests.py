34ef75b235a6e8de9b3c366db06d783c79af3809
BSkrockii <SkrockiBrandon@gmail.com>
2020-02-18 20:38:51 -0700

    pushing one test case

--- getalife/life/tests.py
+++ getalife/life/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+class JenkinsTest(TestCase):
+
+    def test_JenkinsRunOne(self):
+        self.assertEqual(1, 1)
+        print("Test")
