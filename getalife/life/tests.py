6cfc0ce047ebf2fb6467117bc7b38214cd0fd919
BSkrockii <SkrockiBrandon@gmail.com>
2020-02-22 13:51:52 -0700

    further testing

--- getalife/life/tests.py
+++ getalife/life/tests.py
@@ -6,3 +6,7 @@ class JenkinsTest(TestCase):
     def test_JenkinsRunOne(self):
         self.assertEqual(1, 1)
         print("Test")
+
+    def test_JenkinsRuntwo(self):
+        self.assertEqual(1, 1)
+        print("Test")
