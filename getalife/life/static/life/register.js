0b4bbd051f0e668e5269ba5a5460b99707640841
BSkrockii <skrockibrandon@gmail.com>
2020-02-29 18:38:45 -0700

    Made page more user friendly

---
+++ getalife/life/static/life/register.js
@@ -0,0 +1,16 @@
+function mouseoverDown(obj) {
+    var obj = document.getElementById('password');
+    obj.type = "text";
+  }
+  function mouseoutUp(obj) {
+    var obj = document.getElementById('password');
+    obj.type = "password";
+  }
+  function mouseoverDownPassRepear(obj) {
+    var obj = document.getElementById('passrepeat');
+    obj.type = "text";
+  }
+  function mouseoutUpPassRepeat(obj) {
+    var obj = document.getElementById('passrepeat');
+    obj.type = "password";
+  }
