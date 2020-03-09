051509a14d8a94a6bab28e4a90ce56109c52fef1
tate <tate16453@gmail.com>
2020-03-08 18:15:46 -0600

    views for calendar and event

--- getalife/life/views.py
+++ getalife/life/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User, auth
+from datetime import datetime, timedelta, date
+from django.views import generic
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+import calendar
+
+from .models import *
+from .utils import Calendar
+from .forms import EventForm
 
 # Create your views here.
 def index(request):
@@ -61,3 +70,58 @@ def about(request):
 def signOut(request):
     auth.logout(request)
     return redirect('/login')
+
+def calendarFt(request):
+    context = None
+    return render(request, 'life/calendarFt.html', context)
+
+def event(request):
+    context = None
+    return render(request, 'life/event.html', context)
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'life/calendarFt.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d = get_date(self.request.GET.get('day', None))
+        calendar = Calendar(d.year, d.month)
+        html_cal = calendar.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        d = get_date(self.request.GET.get('month', None))
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+    
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('calendarFt'))
+    return render(request, 'life/event.html', {'forms': form})
