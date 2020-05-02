f180ceb787157e5cf110840900deb2f370094993
BSkrockii <skrockibrandon@gmail.com>
2020-05-02 16:40:06 -0600

    Drop down data for selected year

--- getalife/life/static/life/budget.js
+++ getalife/life/static/life/budget.js
@@ -1,1 +1,10 @@
-console.log( $( "#budget-year"));
+function showContent(callerYear) {
+
+    let el = $( "#budget-year-" + callerYear);
+
+    if(el.attr('class') == "content-inactive"){
+        el.addClass('content-active').removeClass('content-inactive');
+    }else{
+        el.addClass('content-inactive').removeClass('content-active');
+    }
+}
